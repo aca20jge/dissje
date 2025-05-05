7586286112dbd0daeccf33c83f7c63b7806ca504
aca20jge <125889265+aca20jge@users.noreply.github.com>
2025-05-05 21:56:27 +0100

    Update imitate.py

--- dissertation/src/imitate.py
+++ dissertation/src/imitate.py
@@ -2,13 +2,12 @@
 
 import rospy
 import cv2
-from sensor_msgs.msg import JointState
+from sensor_msgs.msg import CompressedImage, JointState
 from std_msgs.msg import Float32MultiArray
 from cv_bridge import CvBridge
 import numpy as np
 import os
 import mediapipe as mp
-import time
 import miro2 as miro  # MiRo SDK
 
 class MiRoImitator:
@@ -24,21 +23,21 @@ class MiRoImitator:
         robot_name = os.getenv("MIRO_ROBOT_NAME")
         if not robot_name:
             rospy.logerr("MIRO_ROBOT_NAME not set!")
-            return
+            raise ValueError("MIRO_ROBOT_NAME environment variable is required.")
         topic_base = "/" + robot_name
 
         self.sub_cam = rospy.Subscriber(
             topic_base + "/sensors/caml/compressed",
-            rospy.AnyMsg,
+            CompressedImage,
             self.callback_cam,
             queue_size=1,
             tcp_nodelay=True,
         )
 
-        # Publisher using MiRo SDK style
         self.kinematic_pub = rospy.Publisher(
             topic_base + "/control/kinematic_joints", JointState, queue_size=0
         )
+
         self.cosmetic_pub = rospy.Publisher(
             topic_base + "/control/cosmetic_joints", Float32MultiArray, queue_size=0
         )
@@ -73,9 +72,11 @@ class MiRoImitator:
             left_eye = landmarks[33]
             right_eye = landmarks[263]
 
+            # Estimate yaw from horizontal eye difference
             eye_diff_x = right_eye.x - left_eye.x
             yaw = np.clip(eye_diff_x * 10.0, -0.9, 0.9)
 
+            # Estimate pitch from nose position relative to eyes
             eye_center_y = (left_eye.y + right_eye.y) / 2
             pitch = np.clip((nose_tip.y - eye_center_y) * 10.0, -0.35, 0.1)
 
@@ -84,14 +85,14 @@ class MiRoImitator:
     def set_move_kinematic(self, yaw, pitch):
         joint_cmd = JointState()
 
-        tilt = miro.constants.TILT_RAD_CALIB  # not used
+        tilt = miro.constants.TILT_RAD_CALIB  # fixed (not used)
         lift = miro.constants.LIFT_RAD_CALIB  # neutral lift
 
         joint_cmd.position = [tilt, lift, yaw, pitch]
         self.kinematic_pub.publish(joint_cmd)
 
     def imitate_head_pose(self):
-        print("MiRo is imitating your head pose. Ctrl+C to stop.")
+        rospy.loginfo("MiRo is imitating your head pose.")
         while not rospy.is_shutdown():
             if self.new_frame:
                 self.new_frame = False
@@ -106,7 +107,7 @@ class MiRoImitator:
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
                         cv2.putText(frame, f"Pitch: {pitch:.2f}", (10, 60),
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-                        cv2.imshow("MiRo View", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+                        cv2.imshow("MiRo Head View", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
                         cv2.waitKey(1)
 
             rospy.sleep(self.TICK)
