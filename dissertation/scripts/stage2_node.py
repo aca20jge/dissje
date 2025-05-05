f001d3f739e8168d615badd7a73650a8ea546340
aca20jge <JGEdwards1@sheffield.ac.uk>
2025-05-06 00:29:15 +0100

    nodes didnt work

--- dissertation/scripts/stage2_node.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import rospy
-from imitate_head_pose import ImitateHeadPose
-
-if __name__ == "__main__":
-    rospy.init_node("stage2_node")
-    node = ImitateHeadPose(stage=2)
-    node.run()
